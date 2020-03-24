06a24b000f55f02f70544f375802d79d1d3de554
Greg Johnston <gcjohnston91@gmail.com>
2020-03-24 10:21:41 -0400

    Bonuses from changing weapons now working correctly

--- src/mainView/mainView.js
+++ src/mainView/mainView.js
@@ -2,10 +2,9 @@ import React, {useState, useEffect} from 'react';
 import './mainView.scss'
 import Stats from "../components/Stats/Stats";
 import Equipment from "../components/equipment/Equipment";
-import {weapons, headGear, armor, boots, capes, orbs, nothing} from '../gear';
+import {weapons, headGear, armor, boots, capes, orbs} from '../gear';
 
 const MainView = () => {
-
 
   const [characterBaseStats, setCharacterBaseStats] = useState({
     health: 400,
@@ -16,35 +15,56 @@ const MainView = () => {
     speed: 14
   });
   const [characterStats, setCharacterStats] = useState(characterBaseStats);
-  const [bonuses, setBonuses] = useState({
+  const [baseBonuses, setBaseBonuses] = useState({
     health: 1, attack: 3, defense: 4,
     magic: 1, magicResist: 2, speed: 5
   });
+  const [bonuses, setBonuses] = useState(baseBonuses);
   const equippedGear = [
     weapons[0], armor[0], boots[0], headGear[0], capes[0], orbs[0]
   ];
   const [currentWeapon, setCurrentWeapon] = useState(weapons[0]);
 
+  const applyBonus = () => {
+    setBonuses({
+        health: baseBonuses.health + currentWeapon.health,
+        attack: baseBonuses.attack + currentWeapon.attack,
+        defense: baseBonuses.defense + currentWeapon.defense,
+        magic: baseBonuses.magic + currentWeapon.magic,
+        magicResist: baseBonuses.magicResist + currentWeapon.magicResist,
+        speed: baseBonuses.speed + currentWeapon.speed,
+      }
+    )
+  };
+
   const weaponBonus = () => {
     setCharacterStats({
-      health: currentWeapon.health + characterBaseStats.health,
-      attack: currentWeapon.attack + characterBaseStats.attack,
-      defense: currentWeapon.defense + characterBaseStats.defense,
-      magic: currentWeapon.magic + characterBaseStats.magic,
-      magicResist: currentWeapon.magicResist + characterBaseStats.magicResist,
-      speed: currentWeapon.speed + characterBaseStats.speed
+      health: bonuses.health + characterBaseStats.health,
+      attack: bonuses.attack + characterBaseStats.attack,
+      defense: bonuses.defense + characterBaseStats.defense,
+      magic: bonuses.magic + characterBaseStats.magic,
+      magicResist: bonuses.magicResist + characterBaseStats.magicResist,
+      speed: bonuses.speed + characterBaseStats.speed
     })
   };
 
-  const equipWeapon = (i) => {
+  const equipWeapon = (i, e) => {
+    e.preventDefault();
     equippedGear.splice(0, 1, weapons[i]);
     setCurrentWeapon(equippedGear[0]);
   };
 
   useEffect(() => {
-    console.log(currentWeapon)
+    if (currentWeapon) {
+      applyBonus();
+    }
   }, [currentWeapon]);
 
+  useEffect(() => {
+    if (bonuses) {
+      weaponBonus();
+    }
+  }, [bonuses]);
 
   const updateStats = () => {
     setCharacterStats({
@@ -84,9 +104,9 @@ const MainView = () => {
           <Equipment title='Orb' equipment={equippedGear[5]}/>
         </div>
       </div>
-      <button onClick={() => equipWeapon(0)}>Equip Nothing</button>
-      <button onClick={() => equipWeapon(1)}>Equip Hammer</button>
-      <button onClick={() => equipWeapon(2)}>Equip Sword</button>
+      <button onClick={(e) => equipWeapon(0, e)}>Equip Nothing</button>
+      <button onClick={(e) => equipWeapon(1, e)}>Equip Hammer</button>
+      <button onClick={(e) => equipWeapon(2, e)}>Equip Sword</button>
     </div>
   );
 };
