b44a5cfd924b89bbca9f0c0de12b49cc997a8549
Greg Johnston <gcjohnston91@gmail.com>
2020-03-25 08:20:47 -0400

    Gear bonuses now available through terrible code

--- src/mainView/mainView.js
+++ src/mainView/mainView.js
@@ -20,40 +20,52 @@ const MainView = () => {
     magic: 1, magicResist: 2, speed: 5
   });
   const [bonuses, setBonuses] = useState(baseBonuses);
-  // const equippedGear = [
-  //   weapons[0], armor[0], boots[0], headGear[0], capes[0], orbs[0]
-  // ];
   const [equippedGear, setEquippedGear] = useState({
     weapon: weapons[1], armor: armor[1], boots: boots[1], headGear: headGear[1], cape: capes[1], orb: orbs[1]
   });
-  const [currentWeapon, setCurrentWeapon] = useState(equippedGear.weapon);
-  const [currentArmor, setCurrentArmor] = useState(equippedGear.armor);
-
-  // const weaponBonus = () => {
-  //   setBonuses({
-  //       health: baseBonuses.health + currentWeapon.health,
-  //       attack: baseBonuses.attack + currentWeapon.attack,
-  //       defense: baseBonuses.defense + currentWeapon.defense,
-  //       magic: baseBonuses.magic + currentWeapon.magic,
-  //       magicResist: baseBonuses.magicResist + currentWeapon.magicResist,
-  //       speed: baseBonuses.speed + currentWeapon.speed,
-  //     }
-  //   )
-  // };
 
   const weaponBonus = () => {
     setBonuses({
         health: baseBonuses.health + equippedGear.weapon.health,
-        attack: baseBonuses.attack + currentWeapon.attack,
-        defense: baseBonuses.defense + currentWeapon.defense,
-        magic: baseBonuses.magic + currentWeapon.magic,
-        magicResist: baseBonuses.magicResist + currentWeapon.magicResist,
-        speed: baseBonuses.speed + currentWeapon.speed,
+        attack: baseBonuses.attack + equippedGear.weapon.attack,
+        defense: baseBonuses.defense + equippedGear.weapon.defense,
+        magic: baseBonuses.magic + equippedGear.weapon.magic,
+        magicResist: baseBonuses.magicResist + equippedGear.weapon.magicResist,
+        speed: baseBonuses.speed + equippedGear.weapon.speed,
       }
     )
   };
 
-  const applyBonus = () => {
+  const healthBonus =
+    equippedGear.weapon.health + equippedGear.armor.health + equippedGear.boots.health +
+    equippedGear.headGear.health + equippedGear.cape.health + equippedGear.orb.health;
+
+  const attackBonus =
+    equippedGear.weapon.attack + equippedGear.armor.attack + equippedGear.boots.attack +
+    equippedGear.headGear.attack + equippedGear.cape.attack + equippedGear.orb.attack;
+
+  const defenseBonus =
+    equippedGear.weapon.defense + equippedGear.armor.defense + equippedGear.boots.defense +
+    equippedGear.headGear.defense + equippedGear.cape.defense + equippedGear.orb.defense;
+
+  const magicBonus =
+    equippedGear.weapon.magic + equippedGear.armor.magic + equippedGear.boots.magic +
+    equippedGear.headGear.magic + equippedGear.cape.magic + equippedGear.orb.magic;
+
+  const magicResistBonus =
+    equippedGear.weapon.magicResist + equippedGear.armor.magicResist + equippedGear.boots.magicResist +
+    equippedGear.headGear.magicResist + equippedGear.cape.magicResist + equippedGear.orb.magicResist;
+
+  const speedBonus =
+    equippedGear.weapon.speed + equippedGear.armor.speed + equippedGear.boots.speed +
+    equippedGear.headGear.speed + equippedGear.cape.speed + equippedGear.orb.speed;
+
+  const gearBonuses = {
+    health: healthBonus, attack: attackBonus, defense: defenseBonus,
+    magic: magicBonus, magicResist: magicResistBonus, speed: speedBonus
+  };
+
+  const renderedBonus = () => {
     setCharacterStats({
       health: bonuses.health + characterBaseStats.health,
       attack: bonuses.attack + characterBaseStats.attack,
@@ -72,12 +84,14 @@ const MainView = () => {
   useEffect(() => {
     if (equippedGear) {
       weaponBonus();
+      console.log(gearBonuses)
     }
   }, [equippedGear]);
 
   useEffect(() => {
     if (bonuses) {
-      applyBonus();
+      renderedBonus();
+
     }
   }, [bonuses]);
 
